fdb22ea84c0179182bf2ea6f4c59d8f2c8a6721b
taner94 <yldrmtnr@windowslive.com>
2018-03-11 22:57:04 +0300

    Add files via upload
    list veri yapisi örnegi

---
+++ list veri yapısı/PythonApplication3/PythonApplication3.py
@@ -0,0 +1,31 @@
+l=[1,2,3,4]
+print (l)
+l.append(55,)
+print (l)
+l.insert(2,344)
+print (l)
+l.remove(1)
+print(l)
+print(l.pop())
+print(l)
+print (l.index(4))
+l.append(2)
+l2=[4,3,2]
+l.extend(l2)
+print (l.count(2))
+l.sort()
+print(l)
+l.reverse()
+print(l)
+l3=l2
+print(l3)
+l3.append(600)
+print(l2)
+print(l3)
+l4=l2.copy()
+l4.append(4343)
+print(l4)
+
+
+
+
